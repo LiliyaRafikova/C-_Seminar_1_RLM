572ef4f7eecba43988910f8cf97820dd68e09604
Liliya <liliya89fm@mail.ru>
2022-08-05 11:04:44 +0500

    Example002

---
+++ Examlpe002/Program.cs
@@ -0,0 +1,16 @@
+﻿
+ Console.WriteLine("Программа, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго!");
+Console.WriteLine("Введите первое число: ");
+ int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+if (number_1 == number_2 * number_2)
+        {
+            Console.WriteLine("Ответ: Да, первое число является квадратом второго!");
+        }
+    else
+        {
+            Console.WriteLine("Ответ: Нет, первое число не является квадратом второго!");
+        }
+ 
