7733dbab122e4164f583eeabb1da2865b85e3487
Liliya <liliya89fm@mail.ru>
2022-08-05 10:32:29 +0500

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Программа, которая на вход принимает число и выдаёт его квадрат!");
+Console.Write("Введите любое целое число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number*number;
+Console.Write("Ответ: ");
+Console.WriteLine(result);
